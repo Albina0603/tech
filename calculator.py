96588383e198af84c2ca6442017ab6f79950edda
nurdoolot <yryskeldiydyrysov3@gmail.com>
2025-01-24 13:33:52 +0600

    calc

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print("1. for add")
+print("2. for divide")
+print("3. for multiply")
+print("4. for subtract")
+oper = int(input("Выберите цифру"))
+a = int(input("Выберите первое число:"))
+b = int(input("Выберите второе число:"))
+
+if oper ==1:
+     print({a+b})
+elif oper ==2:
+     print({a/b})
+elif oper ==3:
+     print({a*b})
+elif oper ==4:
+     print({a-b})
+
+
+    
+    
+     
